4a3147a5aa967ddefc74100d5e524c0226851a10
seif essam <seifessam60@gmail.com>
2024-11-26 18:21:18 +0200

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className='min-h-screen w-screen overflow-x-hidden relative'>
+      <Navbar />
       <Hero />
       <About />
     </main>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button = ({ title, id, leftIcon, containerClass }) => {
+const Button = ({ title, id, leftIcon,rightIcon ,containerClass }) => {
   return (
     <button
       className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}
@@ -9,6 +9,7 @@ const Button = ({ title, id, leftIcon, containerClass }) => {
       <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
         <div>{title}</div>
       </span>
+      {rightIcon}
     </button>
   );
 };
